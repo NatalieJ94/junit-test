67c16c69d6c417344aefe757d4fc2fdd0c3aad18
Natalie Aitcheson <natalie.aitcheson@kcl.ac.uk>
2017-01-12 13:00:31 +0000

    Initialise repository

---
+++ MyTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MyTest {
+
+    @Test
+    public void test() {
+        assertEquals("You have installed JUnit correctly if this test returns this error message\n", true,false);
+    }
+}
